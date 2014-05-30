073b3fbf7810c749d3db236cd9086370162ffe69
Steven Branham <spbranham@gmail.com>
2014-05-30 16:48:39 -0600

    Has some issues still
    Prior functionality still works… just can’t get the upload to work

--- src/com/Branham/facebookcam2/SelectActivity.java
+++ src/com/Branham/facebookcam2/SelectActivity.java
@@ -1,23 +1,33 @@
 package com.Branham.facebookcam2;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Spinner;
-import android.widget.Toast;
-
+import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
+import com.facebook.android.Facebook;
 import com.facebook.widget.LoginButton;
 
 /**
@@ -29,6 +39,8 @@ import com.facebook.widget.LoginButton;
  * 	http://stackoverflow.com/questions/20714058/file-exists-and-is-directory-but-listfiles-returns-null
  * 	http://stackoverflow.com/questions/1016896/how-to-get-screen-dimensions	
  * 	http://stackoverflow.com/questions/6814575/image-adapter-adding-imageviews-dynamically-to-gridview-without-cursor
+ * 	http://stackoverflow.com/questions/18257451/how-to-fix-noclassdeffounderror-com-facebook-android-facebook
+ * 	http://stackoverflow.com/questions/19976026/parsing-json-data-in-java-from-facebook-graph-response
  * 
  */
 
@@ -45,15 +57,24 @@ public class SelectActivity extends Activity{
 		};
 		private UiLifecycleHelper uiHelper;
 		private Spinner albums;
+		private List<String> albumNames;
+		private ArrayAdapter adapter;
+		private Facebook mfacebook;
+		private File[] files;
+		private Bitmap[] uploadPics;
 	
 	
 		@Override
 		protected void onCreate(Bundle savedInstanceState){
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.select_activity);
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+			
+			// Get permission to read album titles
 			LoginButton authButton = (LoginButton) findViewById(R.id.authButton);
+			authButton.setReadPermissions(Arrays.asList("public_profile", "user_photos"));
 			
-			
+			// uiHelper is for keeping the Login button updated
 			uiHelper = new UiLifecycleHelper(this,callback);
 			uiHelper.onCreate(savedInstanceState);
 			
@@ -65,29 +86,84 @@ public class SelectActivity extends Activity{
 		
 			String ExternalStorageDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
 			String path = ExternalStorageDirectoryPath + "/FacebookCam2";
-			
-			/*Log.d("This is the path", path);
-			Toast.makeText(getApplicationContext(), path, Toast.LENGTH_LONG).show();*/
-			
 			File targetDir = new File(path);
 			
-			/*Log.d("Files",Environment.getExternalStorageState());
+			/*	Code used to see if the path worked
+			Log.d("Files",Environment.getExternalStorageState());
 			Log.d("Files",targetDir.exists()+"");
 			Log.d("Files",targetDir.isDirectory()+"");
-			Log.d("Files",targetDir.listFiles()+"");*/
+			Log.d("Files",targetDir.listFiles()+"");
+			*/
 			
-			File[] files = targetDir.listFiles();
+			files = targetDir.listFiles();
 			for(int i=0; i<files.length; i++){
 				File file = files[i];
-				Log.d("SelectActivity", file.toString());
 				myImageAdapter.add(file.getAbsolutePath());
 			}
 			
 			/*The Login*/
 			
 			albums = (Spinner)findViewById(R.id.spinner_album);
-			albums.setVisibility(View.GONE);
+			albums.setVisibility(View.GONE);						// Hide spinner while not logged in
 			
+			
+			/*The Upload*/
+			
+			Button upload = (Button) findViewById(R.id.button_upload);
+			upload.setOnClickListener(
+					new View.OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+							
+							// Make sure we have permission to upload
+							if(Session.isPublishPermission("publish_actions")){
+								Log.d("Permission", "Permission");
+							}else{
+								Log.d("Permission", "Getting Permission");
+								Session session = Session.getActiveSession();
+								session.requestNewPublishPermissions(askPermissions());
+							}
+							
+							// Upload photos
+							getPicBitmaps();
+							if(uploadPics.length > 0){
+								for(int i=0; i < uploadPics.length; i++){
+									Bundle params = new Bundle();
+									params.putParcelable("source", uploadPics[i]);
+									new Request(
+											Session.getActiveSession(),
+											"/" + albums.getSelectedItem().toString() + "/photos",
+											params,
+											HttpMethod.POST,
+											new Request.Callback() {
+												
+												@Override
+												public void onCompleted(Response response) {
+													Log.d("Upload", "Upload for a file Completed");
+												}
+											}).executeAsync();
+								}
+							}
+							
+							// Delete photos from phone
+							
+						}
+					}
+			);
+		}
+		
+		public void getPicBitmaps(){
+			uploadPics = new Bitmap[files.length];
+			for(int i = 0; i < files.length; i++){
+				uploadPics[i] = BitmapFactory.decodeFile(files[i].getPath());
+				Log.d("Bitmap", uploadPics[i].toString());
+			}
+		}
+		
+		public Session.NewPermissionsRequest askPermissions(){
+			Session.NewPermissionsRequest newPermissionsRequest = new Session.NewPermissionsRequest(this, "publish_actions");
+			return newPermissionsRequest;
 		}
 		
 		@Override
@@ -100,10 +176,42 @@ public class SelectActivity extends Activity{
 			if (state.isClosed()){
 				Log.d("Session", "Logged out...");
 			}
-			if(state.isOpened()){
+			if(state.isOpened()){  
 				Log.d("Session", "Logged in...");
-				albums.setVisibility(View.VISIBLE);
+				
+				Log.d("Session", "Access token: " + session.getAccessToken());
+		
+				new Request(session, "/me/albums", null, HttpMethod.GET, new Request.Callback() {
+					
+					@Override
+					public void onCompleted(Response response) {
+						try{
+							JSONObject json = new JSONObject(response.getRawResponse());
+							JSONArray jarray = json.getJSONArray("data");
+							albumNames = new ArrayList<String>();
+							for(int i = 0; i < jarray.length(); i++){
+								JSONObject oneAlbum = jarray.getJSONObject(i);
+								if(oneAlbum.getBoolean("can_upload")==true){
+									albumNames.add(oneAlbum.getString("name"));
+								}
+							}
+							
+							updateSpinner();
+						
+						}catch(JSONException e){
+							Log.d("JSON", e.getMessage());
+						}
+					}
+				}).executeAsync();
+				
 			}
+		}
+		
+		// Add Array to arrayAdapter and set the spinner visible
+		public void updateSpinner(){
+			adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, albumNames);
+			albums.setAdapter(adapter);
+			albums.setVisibility(View.VISIBLE);
 		}
 		
 		@Override
